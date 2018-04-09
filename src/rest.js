6c8d8c607c4f0c058c1b2d8aa54f4fc287fb66fd
John Li <lizoya2011@gmail.com>
2018-04-10 09:42:36 +1200

    Update rest.js
    clean up config

--- src/rest.js
+++ src/rest.js
@@ -2,7 +2,6 @@ import axios from 'axios'
 
 // URL and endpoint constants
 const SOURCE_API_URL = `https://newsapi.org/v1/sources?language=en`;
-// API_KEY = `d2779aa7f9f44c2794e7818f3bf60aae`;
 const ARTICLE_API_URL = `https://newsapi.org/v1`;
 
 export const SOURCE = axios.create({
